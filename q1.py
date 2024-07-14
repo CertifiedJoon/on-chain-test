ff464126b45b0e819b548999d31d33f1346b479c
Joon_Thinkpad <mjypark1212@gmail.com>
2024-07-15 08:45:35 +0900

    Add: Final Answer Q1

--- q1.py
+++ q1.py
@@ -1,17 +1,49 @@
 import ccxt
+from pprint import pprint
 
-with open("secret.txt") as f:
-    lines = f.readlines()
-    apiKey = lines[0].strip()
-    secretKey = lines[1].strip()
 
-exchange = ccxt.binance(
-    config={
-        "apiKey": apiKey,
-        "secret": secretKey,
-    }
-)
+class FutureSpotPriceFetcher:
+    def __init__(self, secret_file):
+        with open(secret_file) as f:
+            lines = f.readlines()
+            apiKey = lines[0].strip()
+            secretKey = lines[1].strip()
+            self._exchange = ccxt.binance(
+                config={
+                    "apiKey": apiKey,
+                    "secret": secretKey,
+                }
+            )
+        self._priceMap = {}  # sym : price
 
-tickers = exchange.fetchTickers()
+    def getAllPrice(self):
+        self._updatePriceMap()
+        self._updatePriceMap(spot=False)
+        return self._priceMap
 
-print(tickers)
+    def _updatePriceMap(self, spot=True):
+        priceMap = self.getPriceMap()
+        self.setExchangeToSpot() if spot else self.setExchangeToFutures()
+        tickers = self.getExchange().fetchTickers()
+        for sym, info in tickers.items():
+            if info["ask"] and info["bid"]:
+                priceMap[sym] = info["bid"] + ((info["ask"] - info["bid"]) / 2)
+
+    def getExchange(self):
+        return self._exchange
+
+    def setExchangeToFutures(self):
+        exchange = self.getExchange()
+        exchange.options["defaultType"] = "future"
+
+    def setExchangeToSpot(self):
+        exchange = self.getExchange()
+        exchange.options["defaultType"] = "spot"
+
+    def getPriceMap(self):
+        return self._priceMap
+
+
+if __name__ == "__main__":
+    fetcher = FutureSpotPriceFetcher("secret.txt")
+    pprint(fetcher.getAllPrice())
